d44b295ed6584949fccb48ef776faa5c60086491
vivian079 <zzzyyyjjj010709@gmail.com>
2023-08-06 21:04:50 +0800

    Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider></ChakraBaseProvider>
+      <App />
   </React.StrictMode>,
 )
