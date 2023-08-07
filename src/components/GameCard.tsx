3266d170f0c14ca945065b898534dedecc9eca4b
vivian079 <zzzyyyjjj010709@gmail.com>
2023-08-07 15:33:24 +0800

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,8 +10,9 @@ import {MdPhoneIphone} from 'react-icons/md';
 import {SiNintendo} from 'react-icons/si';
 import {BsGlobe} from 'react-icons/bs';
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from './CriticScore';
 
 interface Props {
   game: Game;
@@ -23,9 +24,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
           platforms={game.parent_platforms.map((p) => p.platform)}
         />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        
       </CardBody>
     </Card>
   );
